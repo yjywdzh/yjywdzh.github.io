9d2103819425c637ba9a7fab27be1460a69d414d
yjywdzh <157541211+yjywdzh@users.noreply.github.com>
2025-03-23 19:47:05 +0000

    Deploying to gh-pages from @ yjywdzh/yjywdzh.github.io@c0a38ac700dd6c253b6cfe66321774e573ab6f8a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-awards",
+          title: "awards",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/awards/";
+          },
         },{id: "post-a-post-with-image-galleries",
         
           title: "a post with image galleries",
